6f62b65e8071a6406f12616de3157a9b68bf54fd
Rebecca Skinner <rebecca@aster.is>
2017-06-16 17:27:55 -0500

    read files from disk and add sample anagram pair

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,13 @@ module Main where
 
 import Data.Char
 import System.Environment
+import Data.List
+import qualified Data.Vector.Unboxed.Mutable as MV
+import qualified Data.Vector.Unboxed as V
+import Control.Monad.Primitive
+import System.IO.Unsafe
+
+type Anagramer = String -> String -> Bool
 
 primes :: [Int]
 primes = sieve [2..]
@@ -9,24 +16,40 @@ primes = sieve [2..]
     sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p > 0 ]
 
 alphabetIndex :: Char -> Int
-alphabetIndex c = (ord . toUpper $ c) - a
-  where a = ord 'A'
+alphabetIndex c = ord c - ord 'A'
 
 toPrime :: Char -> Int
 toPrime = (primes !!) . alphabetIndex
 
 strProduct :: String -> Int
-strProduct = product . map toPrime . filter isLetter
+strProduct = foldl (\prod thisLetter -> prod * toPrime thisLetter) 1
 
-anagram :: String -> String -> Bool
-anagram a b = strProduct a == strProduct b
+anagramPrime :: Anagramer
+anagramPrime a b = strProduct a == strProduct b
 
-anagrams :: [String] -> Bool
-anagrams [] = True
-anagrams [_] = True
-anagrams (x:y:rest) = (anagram x y) && anagrams (y:rest)
+anagramSort :: Anagramer
+anagramSort a b = (sort a) == (sort b)
+
+updateField v c = let idx = alphabetIndex c in do
+  old <- MV.read v idx
+  MV.write v idx (old + 1)
+  return v
+
+anagrams :: Anagramer -> [String] -> Bool
+anagrams _ [] = True
+anagrams _ [_] = True
+anagrams f (x:y:rest) = (f x y) && anagrams f (y:rest)
 
 main :: IO ()
 main = do
-  args <- getArgs
-  print $ anagrams args
+  (method : fileList) <- getArgs
+  files <- mapM readFile fileList
+  let filteredList = map (filter isLetter . map toLower) files
+  let f = getMethod method
+  print $ (`anagrams` filteredList) <$> f
+
+getMethod :: String -> Maybe Anagramer
+getMethod m
+ | m == "prime" = Just anagramPrime
+ | m == "sort" = Just anagramSort
+ | otherwise = Nothing
