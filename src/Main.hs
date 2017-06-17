77405c00b2006c3553c1fd7fa0b9d26b545a6072
Rebecca Skinner <rebecca@rebeccaskinner.net>
2017-06-16 20:19:11 -0500

    begin adding vector based comparison

--- src/Main.hs
+++ src/Main.hs
@@ -3,10 +3,11 @@ module Main where
 import Data.Char
 import System.Environment
 import Data.List
-import qualified Data.Vector.Unboxed.Mutable as MV
-import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector.Unboxed.Mutable as V
+import Data.Vector.Unboxed (freeze)
 import Control.Monad.Primitive
 import System.IO.Unsafe
+import Control.Monad
 
 type Anagramer = String -> String -> Bool
 
@@ -30,6 +31,28 @@ anagramPrime a b = strProduct a == strProduct b
 anagramSort :: Anagramer
 anagramSort a b = (sort a) == (sort b)
 
+anagramVector :: Anagramer
+anagramVector a b = unsafePerformIO $ do
+  emptyVect <- newEmpty 26
+  added <- foldM (updateVect (+1)) emptyVect a
+  subed <- foldM (updateVect ((-)1)) added b
+  final <- freeze subed
+  expected <- freeze emptyVect
+  return $ expected == final
+
+newEmpty :: Int -> IO (V.MVector (PrimState IO) Int)
+newEmpty size = do
+  v <- V.new size
+  V.set v 0
+  return v
+
+updateVect :: (Int -> Int) -> V.MVector (PrimState IO) Int -> Char -> IO (V.MVector (PrimState IO) Int)
+updateVect f v c =
+  let idx = alphabetIndex c in do
+    oldVal <- V.read v idx
+    V.write v idx (f oldVal)
+    return v
+
 anagrams :: Anagramer -> [String] -> Bool
 anagrams _ [] = True
 anagrams _ [_] = True
@@ -43,8 +66,9 @@ main = do
   let f = getMethod method
   print $ (`anagrams` filteredList) <$> f
 
-getMethod :: String -> Maybe Anagramer
+getMethod :: String -> Either String Anagramer
 getMethod m
- | m == "prime" = Just anagramPrime
- | m == "sort" = Just anagramSort
- | otherwise = Nothing
+ | m == "prime" = Right anagramPrime
+ | m == "sort" = Right anagramSort
+ | m == "vector" = Right anagramVector
+ | otherwise = Left "unknown solver strategy"
